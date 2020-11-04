26222a7613ee2bb150a8e8d4f218bf16ba5db191
bulidiriba <bulidiriba@gmail.com>
2020-11-04 10:48:09 +0300

    enable CORS in the flask api so that result will be returned without Network Error

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # app.py - a flask api using flask_restful 
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from numpyencoder import NumpyEncoder
 
 from utils import *
@@ -7,6 +8,7 @@ from utils import *
 import json
 
 app = Flask(__name__)
+CORS(app) # to enable CORS for the routes, unless from the front end response becomes Network Error
 
 @app.route('/get_fake_news_score', methods=['POST', 'GET'])
 def get_fake_news_score():
